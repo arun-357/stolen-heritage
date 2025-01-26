2538ccb10fcfbdc4649d8d8f251b53a06e5ee24e
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-26 17:57:43 +0530

    add:loader add:navBar

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/80 backdrop-blur-md">
+      <div className="max-w-8xl mx-auto flex justify-center items-center py-10">
+        <h1 className="text-white font-serif text-4xl tracking-wider relative inline-block group">
+          STOLEN HERITAGE
+          <span className="underline top-[-5px] origin-left"></span>
+          <span className="underline bottom-[-5px] origin-right "></span>
+        </h1>
+      </div>
+    </header>
+  );
+}
